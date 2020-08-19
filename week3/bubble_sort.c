8181d2541be9118e6823f109d749c59ecbf2d74b
chandralekha-kotha <69664021+chandralekha-kotha@users.noreply.github.com>
2020-08-19 16:20:49 +0530

    Add files via upload

---
+++ week3/bubble_sort.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+void bubble_sort(int a[50] , int n)
+{
+	int i, j, t;
+	for(i=0;i<n-1;i++)
+	{	
+		for(j=0;j<n-i-1;j++)
+		{
+			if(a[j] > a[j+1])
+			{
+				t = a[j];
+				a[j] = a[j+1];
+				a[j+1] = t;
+			}
+		}
+	}
+}	
+int main()
+{
+	int n, i;
+	scanf("%d",&n);
+	int a[n];
+	for(i=0;i<n;i++)
+		scanf("%d",&a[i]);
+	bubble_sort(a,n);
+	for(i=0;i<n;i++)
+	printf("%d\t",a[i]);
+	
+}
